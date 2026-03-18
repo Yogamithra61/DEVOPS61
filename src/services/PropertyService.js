00b6e5e68d23bb96c2af14caecb1ad1cdc33b657
Yogamithra M <23suca61@tcarts.in>
2026-03-18 20:15:02 +0530

    Updated PropertyService.js to fix ESLint no-anonymous-default-export warning.

---
+++ src/services/PropertyService.js
@@ -0,0 +1,9 @@
+// PropertyService.js
+
+class PropertyService {
+    // ... class methods ...
+}
+
+const propertyService = new PropertyService();
+// Updated the export to fix ESLint warning
+export default propertyService;
